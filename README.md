dc399a18463d11990d4d2677fb3c54316a8f0bdc
Zhaoshan-Duan <joshua.duan47@gmail.com>
2024-03-08 09:47:46 -0800

    finished version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Read Me
+
+The Learn Together app displays a list of articles about several Jetpack libraries. 
+
+Users can choose the topic of their choice and learn about its latest developments.
--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TutorialPage()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TutorialContent(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+    ) {
+   Column(modifier = modifier){
+       Image( painter = imagePainter, contentDescription = null,)
+       Text(
+           text = title,
+           fontSize = 24.sp,
+           modifier = Modifier.padding(16.dp)
+       )
+       Text(
+           text = shortDescription,
+           modifier = Modifier
+               .padding(
+               start = 16.dp,
+               end = 16.dp
+               ),
+           textAlign = TextAlign.Justify
+       )
+       Text(
+           text = longDescription,
+           modifier = Modifier.padding(16.dp),
+           textAlign = TextAlign.Justify
+       )
+
+   }
 }
 
+@Composable
+fun TutorialPage(modifier: Modifier = Modifier) {
+    TutorialContent(
+        title = stringResource(R.string.jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_description),
+        longDescription = stringResource(R.string.tutorial_description),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TutorialPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        TutorialPage()
     }
+
 }
